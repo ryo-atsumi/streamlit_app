8024488a85a4d021dc71167371630b2f61c64d88
ryo-atsumi <doskoipanda817@gmail.com>
2022-10-24 19:40:39 +0900

    初期追加

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from PIL import Image
+import time
+
+
+
+st.title('Streamlit 入門')
+
+st.write('IMAGE')
+
+st.selectbox(
+    '',
+
+)
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('/Users/atsumiryo/Desktop/初期画面.png')
+#     st.image(img, caption='11111111', use_column_width=True)
+
